9cf14ed4b69332eb8ada695fc060b168c0490786
andygimma <andy.n.gimma@gmail.com>
2014-10-15 21:27:12 -0400

    basic templates

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,22 +1,40 @@
 class LocationsController < ApplicationController
   def new
+    @location = Location.new
   end
 
   def create
+    @location= Location.create(location_params)
+    redirect_to locations_path
   end
 
   def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    redirect_to locations_path(@location) 
   end
 
   def edit
+    @location = Location.find(params["id"]) 
   end
 
   def destroy
+    @location = Location.find(params["id"])
+    @location.destroy
+    redirect_to locations_path
   end
 
   def show
+    @location = Location.find(params[:id])
   end
 
   def index
+    @locations = Location.all # TODO paginate, make searchable
   end
+  
+  private
+
+    def location_params
+      params.require(:location).permit(:name, :address, :city, :state, :phone1, :email)
+    end
 end
