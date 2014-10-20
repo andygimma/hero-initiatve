51101df335e2033aee737125e49636fe2a75bdce
andygimma <andy.n.gimma@gmail.com>
2014-10-20 19:44:21 -0400

    header-bg

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,9 @@ module Hero2
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config/application.rb
+    config.assets.precompile += [
+      'header-bg.jpg'
+    ]
   end
 end
