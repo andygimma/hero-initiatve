bbf2984e0cde412432fb15b4f5db5c9b9530925c
andygimma <andy.n.gimma@gmail.com>
2014-10-26 21:15:48 -0400

    edit error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
   
   def welcome_email(email)
+    bingind.pry
     mail(to: email, subject: "Test")
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false\
+  config.active_record.dump_schema_after_migration = false
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
