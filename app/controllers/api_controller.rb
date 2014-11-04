b0bccbf4cb3ce7244405850d04fab16458277c10
andygimma <andy.n.gimma@gmail.com>
2014-11-03 23:31:48 -0500

    add location data to workshops api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,9 +5,8 @@ class ApiController < ApplicationController
   end
 
   def workshops
-    some_date = Date.yesterday
     @workshops = Workshop.where("start_time >= :date", date: 1.day.ago).order(start_time: :desc)
-    render :json => @workshops
+    render :json => @workshops, :include => :location
   end
 
   def map
@@ -16,13 +15,13 @@ class ApiController < ApplicationController
   end
 
   def sign_up
-    @follower= Follower.create(follower_params)
-    @response = {"message" => "Thank you for signing up"}
-    render :json => @response
+#     @follower= Follower.create(follower_params)
+#     @response = {"message" => "Thank you for signing up"}
+#     render :json => @response
   end
   
-  private
-    def follower_params
-      params.require(:email)
-    end
+#   private
+#     def follower_params
+#       params.require(:email)
+#     end
 end
