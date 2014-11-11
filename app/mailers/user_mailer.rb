e40b80e5afc119c16d2940a129e1dc7bceb804c1
andygimma <andy.n.gimma@gmail.com>
2014-11-10 20:16:18 -0500

    set up newsletter

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,12 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "admin@theheroinitiative.org"
   
   def welcome_email(email)
     # TODO Send to multiple emails
     # Create a subject that depends on the day?
     # Create content that includes content, and includes a list of workshops for the week.
     mail(to: email, subject: "Test")
+    @workshops = Workshop.paginate.where("start_time >= :date and end_time <= :date2", date: 1.day.ago, date2: 7.days.from_now).order(start_time: :desc)
+
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -3,7 +3,7 @@ namespace :newsletter do
     t = Time.now
     if t.tuesday? or t.thursday? or t.sunday?
       puts "sending email"
-      UserMailer.welcome_email("andy.n.gimma@gmail.com").deliver
+      UserMailer.welcome_email(["andy.n.gimma@gmail.com", "adaptdroid@gmail.com"]).deliver
       puts "done"
     else 
       puts "not sending email, wrong day of the week"
