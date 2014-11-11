5be8368d614ebd4f4e04798ae168ccc79bfdd85b
andygimma <andy.n.gimma@gmail.com>
2014-11-10 20:37:46 -0500

    2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,10 +5,10 @@ class UserMailer < ActionMailer::Base
     # TODO Send to multiple emails
     # Create a subject that depends on the day?
     # Create content that includes content, and includes a list of workshops for the week.
+    @workshops = Workshop.where("start_time >= ? AND end_time <= ?", 1.day.ago, 7.days.from_now)
     mail(to: email, subject: "Test")
 #     @workshops = Workshop.all
     
-    @workshops = Workshop.where("start_time >= ? AND end_time <= ?", 1.day.ago, 7.days.from_now)
         
 #         Workshop.where("start_time >= :date and end_time <= :date2", date: 1.day.ago, date2: 7.days.from_now).order(start_time: :desc)
 
