34624e248e36b8f55bb6c569ef97c35cf940859c
andygimma <andy.n.gimma@gmail.com>
2014-11-12 18:47:59 -0500

    .

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,6 @@
 class StaticPagesController < ApplicationController
   layout "static", only: [:index]
   def index
-    @users = User.all
-    @users.each do |user|
-      user.admin = true
-      user.save
-    end
   end
   def about
   end
